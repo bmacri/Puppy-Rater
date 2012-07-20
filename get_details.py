ddf8505ebe3dfc9078985d666c56581878e5b618
Bethany Macri <bethany.macri@gmail.com>
2012-07-20 16:16:14 -0400

    All tests pass

--- get_details.py
+++ get_details.py
@@ -1,114 +1,149 @@
-from superclass_GetDogs import GetDogs
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-		     abort, render_template, flash
 import re
+import requests
+import settings
+import sqlite3
+
+from superclass_GetDogs import GetDogs
 
 class DogDetails(GetDogs):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
+        
+        
+    def trim_contents_singledog(self, contents):
+        begin_index = contents.find('<h1 class="title"')
+        end_index = contents.find('<div id="animal_profile_logos">', begin_index + 1)
+        trimmed_contents = contents[begin_index:end_index] 
+        return trimmed_contents
+        
 
+    def individual_dog_urls(self, contents):
+        domain = "http://www.sfspca.org"
+        dog_url_list = []
+        begin_index = contents.find('<div class="view-content">')
+        end_index = contents.find('<div class="item-list"><ul class="pager"><li class="pager-current first">1</li>', begin_index + 1)
+        trimmed_contents = contents[begin_index:end_index] 
+        animal_name_class = trimmed_contents.find('"views-field-field-animal-name-value"')
+        if animal_name_class == -1:
+            return dog_url_list
+        link_tag = trimmed_contents.find('<a href=', animal_name_class + 1)
+        start_quote = trimmed_contents.find('"', link_tag + 1)
+        end_quote = trimmed_contents.find('"', start_quote + 1)
+        next_url = domain + trimmed_contents[start_quote + 1:end_quote]
+        dog_url_list.append(next_url)
+        rest_of_urls = self.individual_dog_urls(trimmed_contents[end_quote + 1:])
+        dog_url_list.extend(rest_of_urls)
+        return dog_url_list
+        
+    def dog_image(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        begin_image_section = trimmed_contents.find('<h1 class')
+        end_image_section = trimmed_contents.find('NAME:', begin_image_section + 1)
+        image_section = trimmed_contents[begin_image_section:end_image_section]
+        begin_image_index = image_section.find('src=')
+        end_image_index = image_section.find('"', begin_image_index + 6)
+        image = image_section[begin_image_index + 5:end_image_index]
+        return image
+        
+            
+    def dog_name(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_name_index = trimmed_contents.find('NAME:')
+        trimmed_contents = trimmed_contents[find_name_index:]
+        name = re.search("[A-Z]+[a-z]+", trimmed_contents)
+        if name:
+            name = name.group()
+            return name
+        return None
 
-	def trim_contents(self, contents):
-		begin_index = contents.find('<div class="view-content">')
-        end_index = contents.find('<div class="item-list"><ul class="pager"><li class="pager-current first">1</li>')
-	    trimmed_contents = contents[begin_index:end_index] 
-		return trimmed_contents
-
-
-	def dog_name(self, contents):
-		contents = self.trim_contents(contents)
-		begin_name_class_index = contents.find('"views-field-field-animal-name-value">')
-		end_name_class_index = contents.find('</a></span>')
-		name_class = contents[begin_name_class_index:end_name_class_index + 1]
-		name = re.search("[A-z]+[a-z]+", contents)
-		return name
-
-
-	def dog_id(self, contents):
-		contents = self.trim_contents(contents)
-	    begin_id_class_index = contents.find('views-field-field-id-value">')
-        end_id_class_index = contents.find('</a></span>')
-        id_class = contents[begin_id_class_index:end_id_class_index + 1]
-	    spca_id = re.search("[0-9]+", contents)
+    def dog_id(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_id_index = trimmed_contents.find('ID:')
+        trimmed_contents = trimmed_contents[find_id_index:]
+        spca_id = re.search("[0-9]+", trimmed_contents)
         if spca_id:
-	        spca_id = spca_id.group()
+            spca_id = spca_id.group()
             return spca_id
-        else:
-            return None
+        return None
+        
 
-
-	def dog_gender(self, contents):
-		contents = self.trim_contents(contents)
-	    begin_gender_class_index = contents.find('"views-field-field-sex-value">')
-        end_gender_class_index = contents.find('</a></span>')
-		gender_class = contents[begin_gender_class_index:end_gender_class_index + 1]
-        gender = re.search("[A-z]+[a-z]+", contents)    
+    def dog_gender(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_gender_index = trimmed_contents.find('GENDER:')
+        trimmed_contents = trimmed_contents[find_gender_index:]
+        gender = re.search("[A-Z]+[a-z]+", trimmed_contents)
         if gender:
             gender = gender.group()
             return gender
-        else:
-		    return None
+        return None
 
 
-	def dog_breed(self, contents):
-		contents = self.trim_contents(contents)
-		begin_id_class_index = contents.find('"views-label-field-primarybreed-value">')
-	    end_id_class_index = contents.find('</a></span>')
-        id_class = contents[begin_id_class_index:end_name_id_index + 1]
-        id = re.search("[A-z]+[a-z]+", contents)
-        if gender:
-	        gender = gender.group()
-            return gender
+    def dog_breed(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_breed_index = trimmed_contents.find('BREED:')
+        trimmed_contents = trimmed_contents[find_breed_index:]
+        breed = re.search('<p class="ap_attr">([^<>]+?)<', trimmed_contents)
+        if breed:
+            breed = breed.group(1)
+            return breed
+        return None
+        
+    
+    def dog_color(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_color_index = trimmed_contents.find('COLOR:')
+        trimmed_contents = trimmed_contents[find_color_index:]
+        color = re.search("[A-Z]+[a-z]+", trimmed_contents)
+        if color:
+            color = color.group()
+            return color
+        return None
+            
+    
+    def dog_age(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        find_age_index = trimmed_contents.find('AGE:')
+        trimmed_contents = trimmed_contents[find_age_index:]
+        age = re.search("([0-9]+y) ([0-9]+m)", trimmed_contents)
+        if age:
+            age = age.group()
+            return age
         else:
             return None
 
 
-	def dog_age(self, contents):
-		contents = self.trim_contents(contents)
-		begin_age_class_index = contents.find('')
-		end_age_class_index = contents.find('')
-		age_class = contents[begin_age_class_index:end_age_class_index]
-		age = re.search("[0-9]+y [0-9]+m", contents) #how to match one letter?  y for year and m for month
-		if age:
-			age = age.group()
-			return age
-		else:
-			return None
+    def dog_description(self, contents):
+        trimmed_contents = self.trim_contents_singledog(contents)
+        begin_description = trimmed_contents.find('"pet-detail">')
+        end_description = trimmed_contents.find('</div>', begin_description + 1)
+        description = trimmed_contents[begin_description + 13:end_description]
+        return description
 
 
-	def dog_color(self, contents):
-		contents = self.trim_contents(contents)
-		begin_photo_field = contents.find('class="views-field-field-photo-fid">')
-		single_dog_begin_index = contents.find('a href=', begin_photo_field + 1)
-		single_dog_end_index = contents.find('"', single_dog_begin_index + 8)
-		url = 'http://www.sfspca.org' + contents[single_dog_begin_index + 8: single_dog_end_index]
-		contents = GetDogs.request_url_contents(url)
-		begin_color_class = contents.find('<p class="ap_type">COLOR:</p>')
-		find_attr_index = contents.find('ap_attr', begin_color_Class + 1)
-		end_attr_index = contents.find('</p>', find_attr_index + 1)
-		color = contents[find_attr_index + 9:end_attr_index]
-		return color
 
 
-	def dog_description(self, contents):
-		contents = self.trim_contents(contents)
-	    begin_photo_field = contents.find('class="views-field-field-photo-fid">')
-        single_dog_begin_index = contents.find('a href=', begin_photo_field + 1)
-        single_dog_end_index = contents.find('"', single_dog_begin_index + 8)
-        url = 'http://www.sfspca.org' + contents[single_dog_begin_index + 8: single_dog_end_index]
-		contents = GetDogs.request_url_contents(url)
-		begin_description_class = contents.find('"pet-detail">')
-		end_description_class = contents.find('</div>', begin_description_class + 1)
-		description = contents[begin_description + 11:end_description] 
-		return description
+#-----------------------------------------------------------------------------------------------
+dog = DogDetails()
+
+contents = dog.file_contents(settings.project_path + 'alldogs.html')
+
+dog_url_list = dog.individual_dog_urls(contents)
+assert dog_url_list[0] == 'http://www.sfspca.org/adoptions/pet-details/10424952-1', dog_url_list[0]
+
+contents = dog.file_contents(settings.project_path + 'singledog.html')
+single_dog_dict = dog.dog_details(contents)
+
+assert single_dog_dict['image'] == 'http://www.sfspca.org/sites/default/files/imagecache/animal_profile_default/photos/55513a35-c8c4-4b35-acd8-e90a05746766.jpg', single_dog_dict['image']
+assert single_dog_dict['name'] == 'Lychee', single_dog_dict['name']
+assert single_dog_dict['spca_id'] == '10424952', single_dog_dict['spca_id']
+assert single_dog_dict['gender'] == 'Female', single_dog_dict['gender']
+assert single_dog_dict['breed'] == 'Chihuahua, Short Coat', single_dog_dict['breed']
+assert single_dog_dict['color'] == 'Tan', single_dog_dict['color']
+assert single_dog_dict['age'] == '3y 8m', single_dog_dict['age']
+assert single_dog_dict['description'] == 'Lychee is a friendly, curious, somewhat shy at first lady who&#39;s heart&#39;s desire is to be someone&#39;s constant friend. She can get a bit overwhelmed at too much noise so would prefer someone who is more book-worm than rock-star.  She is a volunteer favorite for her affectionate personality and stellar leash manners.  She would love to be the only dog in her household.', single_dog_dict['description']
+
+detail_dict = dog.dog_details(contents)
+dog_details_to_db(contents)
 
 
-	def dog_image(self, contents):
-		contents = self.trim_contents(contents)
-		begin_image_class_index = contents.find('<div class="views-field-field-photo-fid">')
-		image_source_begin_index = contents.find('<img src="', begin_image_class_index)
-		image_source_end_index = contents.find('"', image_source_begin_index + 10)
-		image = contents[image_source_begin_index + 10:image_source_end_index]
-		return image
--- superclass_GetDogs.py
+++ superclass_GetDogs.py
@@ -1,53 +1,62 @@
-from datetime import date
-from flask import Flask
 import MySQLdb
 import requests
 import settings
 
-app = Flask(__name__)
+from datetime import date
 
-class GetDogs()
-	def __init__():
-		pass
+class GetDogs():
+    def __init__():
+        pass
 
-	def request_url_contents(self, url):
-		r = request.get(url)
-		return r
+    def requests_url_contents(self, url):
+        r = requests.get(url)
+        return r
 
-	def create_filename(self, url):
-		now = datetime.datetime.now()
-		filename = url + now
-		return filename
+    def create_filename(self, url):
+        now = datetime.datetime.now()
+        filename = url + now
+        return filename
+        
+    def save_contents(self, url):
+        r = requests_url_contents(url)
+        contents = r.text
+        filename = create_filename(url)
+        f = open(filename, 'w')
+        f.write(contents)
+        f.close
 
-	def save_contents(self, url):
-		r = request_url_contents(url)
-		contents = r.text
-		filename = create_filename(url)
-		f = open(filename, 'w')
-		f.write(contents)
-		f.close
-	
-	def dog_details(self, contents):
-		detail_dict = {}
-		detail_dict['name'] = self.dog_name(contents)
-		detail_dict['spca_id'] = self.dog_id(contents)
-		detail_dict['gender'] = self.dog_gender(contents)
-		detail_dict['breed'] = self.dog_breed(contents)
-		detail_dict['color'] = self.dog_color(contents)
-		detail_dict['age'] = self.dog_age(contents)
-		detail_dict['description'] = self.dog_description(contents)
-		detail_dict['image'] = self.dog_image(contents)
-		return detail_dict
+    def file_contents(self,filename):
+        f = open(filename, 'r')
+        contents = f.read()
+        f.close()
+        return contents
 
-	def dog_detailts_to_db(self, detail_dict):
-		conn = conn = MySQLdb.connect(host= "localhost",
-			                          user="root",
-				                      passwd = settings.db_password,
-				                      db="dogdetailsdb")
-	    cursor = conn.cursor()        
-        cursor.execute (" SELECT * FROM  WHERE product_id = %s AND retailer_id = %s ", (product_dict['product_id'],product_dict['retailer_id']))
+    def return_webpage_contents(self,url):
+        r = self.requests_url_contents(url)
+        filename = self.create_filename(url)
+        self.save_contents(url)
+        contents = self.file_contents(filename)
+        return contents
+
+
+    def dog_details(self, contents):
+        detail_dict = {}
+        detail_dict['name'] = self.dog_name(contents)
+        detail_dict['spca_id'] = self.dog_id(contents)
+        detail_dict['gender'] = self.dog_gender(contents)
+        detail_dict['breed'] = self.dog_breed(contents)
+        detail_dict['color'] = self.dog_color(contents)
+        detail_dict['age'] = self.dog_age(contents)
+        detail_dict['description'] = self.dog_description(contents)
+        detail_dict['image'] = self.dog_image(contents)
+        return detail_dict
+
+    def dog_details_to_db(self, detail_dict):
+        conn = MySQLdb.connect(host= "localhost",
+                                user="root",
+                                passwd = settings.db_password,
+                                db="spcadogs")
+        cursor = conn.cursor()
         rows = cursor.fetchall()
-	    if len(rows) == 0: 
-	        cursor.execute (" INSERT INTO details (name,spca_id,gender,breed,color,age,description,image) VALUES (%s,%s,%s,%s,%s,%s) ", (detail_dict['name'],detail_dict['spca_id'],detail_dict['gender'],detail_dict['breed'],detail_dict['color'], detail_dict['age'],detail_dict['description'],detail_dict['image']))
-
-														
+        if len(rows) == 0: 
+            cursor.execute (" INSERT INTO details (name,spca_id,gender,breed,color,age,description,image) VALUES (%s,%s,%s,%s,%s,%s) ", (detail_dict['name'],detail_dict['spca_id'],detail_dict['gender'],detail_dict['breed'],detail_dict['color'], detail_dict['age'],detail_dict['description'],detail_dict['image']))
