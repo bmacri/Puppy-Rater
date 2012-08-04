44bff2988edd041e118163d77be2338ed57acf49
Bethany Macri <bethany.macri@gmail.com>
2012-08-04 12:43:14 -0400

    Commit before forking to work on features

--- superclass_GetDogs.py
+++ superclass_GetDogs.py
@@ -6,6 +6,12 @@ import settings
 
 path = settings.project_path + 'pupfiles/'
 
+class DogObject():
+    pass
+
+class RatingsObject():
+    pass
+
 class GetDogs():
     def __init__():
         pass
@@ -60,46 +66,69 @@ class GetDogs():
         #print detail_dict
         return detail_dict
     
-
-    def dog_details_to_db(self, detail_dict):
-        conn = MySQLdb.connect(host = "localhost",
-                                user ="root",
-                                passwd = settings.db_password,
-                                db ="spcadogs")
-        cursor = conn.cursor()
-        cursor.execute ("INSERT INTO dogdetails (name, spca_id, gender, breed, color, age, description, image) VALUES (%(name)s, %(spca_id)s, %(gender)s, %(breed)s, %(color)s, %(age)s, %(description)s, %(image)s);", detail_dict)
-        conn.commit()
-        
-    def details_from_db(self):
+    def mysql_get_cursor(self):
         conn = MySQLdb.connect(host= "localhost",
                                 user="root",
                                 passwd = settings.db_password,
                                 db="spcadogs")
         cursor = conn.cursor()
+        return conn, cursor
+        
+
+    def dog_details_to_db(self, detail_dict):
+        conn, cursor = self.mysql_get_cursor()
+        cursor.execute ("INSERT INTO dogdetails (name, spca_id, gender, breed, color, age, description, image) VALUES (%(name)s, %(spca_id)s, %(gender)s, %(breed)s, %(color)s, %(age)s, %(description)s, %(image)s);", detail_dict)
+        conn.commit()
+
+     
+    def details_from_db(self):
+        conn, cursor = self.mysql_get_cursor()
         cursor.execute ("SELECT * FROM dogdetails;")
         all_db_data = cursor.fetchall()     
         conn.commit()
-        return all_db_data
+        dog_info_list = []
+        for each_dog in all_db_data:
+            db_dog = DogObject()
+            db_dog.name = each_dog[0]
+            db_dog.spca_id = each_dog[1]
+            db_dog.gender = each_dog[2]
+            db_dog.breed = each_dog[3]
+            db_dog.color = each_dog[4]
+            db_dog.age = each_dog[5]
+            db_dog.description = each_dog[6]
+            db_dog.image = each_dog[7]
+            db_dog.ratings_list = self.select_ratings(db_dog.spca_id) 
+            print db_dog.ratings_list 
+            dog_info_list.append(db_dog)
+        return dog_info_list
         
-    def insert_comments(self, comments_dict):
-        conn = MySQLdb.connect(host = "localhost",
-                             user = "root",
-                             passwd = settings.db_password,
-                             db = "spcadogs")
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO dogratings (comments) VALUES (%(comments_input)s);", comments_dict)
-        conn.commit()
         
-    def select_comments(self):
-        conn = MySQLdb.connect(host= "localhost",
-                                user="root",
-                                passwd = settings.db_password,
-                                db="spcadogs")
-        cursor = conn.cursor()
-        cursor.execute ("SELECT * FROM dogratings;")
-        comments = cursor.fetchall()     
+    def insert_ratings_dict(self, ratings_dict):
+        conn, cursor = self.mysql_get_cursor()
+        cursor.execute("INSERT INTO ratings (spca_id, cuteness, personality, comment) VALUES (%(spca_id)s, %(cuteness)s, %(personality)s, %(comment)s);", ratings_dict)
         conn.commit()
-        return comments
+                
+        
+    def select_ratings(self, spca_id):
+        conn, cursor = self.mysql_get_cursor()
+        cursor.execute ("SELECT * FROM ratings WHERE spca_id = "+str(spca_id)+";")
+        comments = cursor.fetchall()    
+        conn.commit()
+        ratings_list = []
+        for rating in comments: #for row in ratings table where spca_id == parameter
+            ratings_object = RatingsObject()
+            ratings_object.cuteness = rating[2]
+            ratings_object.personality = rating[3]
+            ratings_object.comment = rating[4]
+            ratings_list.append(ratings_object)
+        print ratings_list
+        return ratings_list
+        
+            
+            
+        
+        
+        
 
         
         
--- view.py
+++ view.py
@@ -31,6 +31,7 @@ def login():
             error = 'Invalid password'
         else:
             session['logged_in'] = True
+            session['username'] = request.form['username'] #session carries username information
             return redirect(url_for('show_all_dogs'))
     return render_template('login.html', error=error)
     
@@ -42,12 +43,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/')
-def show_comments():
-    comments = dog.get_comments()
-    print comments
-    return render_template('template.html', comments=comments)
-
 @app.route('/add', methods=['POST'])
 def add_comment():
     if not session.get('logged_in'):
@@ -55,29 +50,18 @@ def add_comment():
         abort(401)        
     if request.method == 'POST':
         ratings_dict = {}
-        ratings_dict['spca_id,' (cuteness, personality, comments)] = spca_id, (request.form["Cuteness Rating"], request.form["Personality Rating"], request.form['text'] 
-        
-        dog.insert_comments(comments_dict)
+        ratings_dict['spca_id']  = request.form["spca_id"]
+        ratings_dict['cuteness'] = request.form["Cuteness Rating"]
+        ratings_dict['personality'] =  request.form["Personality Rating"]
+        ratings_dict['comment'] = request.form['text']
+        dog.insert_ratings_dict(ratings_dict)
         flash('Your comment has been saved')
-    return redirect(url_for('show_comments'))
+    return redirect(url_for('show_all_dogs'))
        
 
 @app.route('/dogs', methods=['GET','POST'])
 def show_all_dogs():
-    dog_info_list = []
-    all_db_data = dog.details_from_db()
-    for each_dog in all_db_data:
-        db_dog = DogFromDB()
-        db_dog.name = each_dog[0]
-        db_dog.spca_id = each_dog[1]
-        db_dog.gender = each_dog[2]
-        db_dog.breed = each_dog[3]
-        db_dog.color = each_dog[4]
-        db_dog.age = each_dog[5]
-        db_dog.description = each_dog[6]
-        db_dog.image = each_dog[7]
-        dog_info_list.append(db_dog)
-    #if dog.select_comments() !
+    dog_info_list = dog.details_from_db()
     return render_template('template.html', db_dogs=dog_info_list)
   
 @app.route('/test', methods=['POST'])
